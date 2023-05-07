402a729d79174b38ebac248cd6dadcb25bb4e6c9
Jonathan Luis Rodriguez <jonathanlsr9611@gmail.com>
2023-05-06 23:48:51 -0400

    adding the css configuration

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { makeStyles } from '@mui/material/styles';
+
+const useStyles = makeStyles((theme) => ({
+    video: {
+        width: '550px',
+        [theme.breakpoints.down('xs')]: {
+            width: '300px',
+        },
+    },
+    gridContainer: {
+        justifyContent: 'center',
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: 'column',
+        },
+    },
+    paper: {
+        padding: '10px',
+        border: '2px solid black',
+        margin: '10px',
+    },
+}));
+
+const VideoPlayer = () => {
+    return (
+        <div>VideoPlayer</div>
+    )
+}
+
+export default VideoPlayer
